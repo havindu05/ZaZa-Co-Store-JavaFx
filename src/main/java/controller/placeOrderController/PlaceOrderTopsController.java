f5d643cb1cf3f976ef2f3ab600d8b41f7a107fd4
Havindu Binduhewa <116324272+havindu05@users.noreply.github.com>
2025-11-30 10:25:37 +0530

    Modified place order tops page

--- src/main/java/controller/placeOrderController/PlaceOrderTopsController.java
+++ src/main/java/controller/placeOrderController/PlaceOrderTopsController.java
@@ -1,4 +1,93 @@
 package controller.placeOrderController;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class PlaceOrderTopsController {
+
+    @FXML
+    void btnAddToCart(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnBuyNow(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnCustomer(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDashBoard(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnEmployee(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnFrocks(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnLogOut(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnOrders(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnPants(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnPlaceOrder(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnProduct(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnReturn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSearch(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSkirts(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSupplier(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnTops(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnTshirts(ActionEvent event) {
+
+    }
+
 }
